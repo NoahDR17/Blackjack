672bb7ff91d60173cdb3071ffd424541c7d110ae
NoahDR17 <noahdavisreynolds@hotmail.com>
2024-05-07 15:15:50 +0100

    Feat: Create basic home page, with working links to other pages using js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var gameLink = document.getElementById("game-link");
+
+gameLink.addEventListener("click", function () {
+    window.location.href = "game.html";});
+
+var rulesLink = document.getElementById("rules-link");
+
+rulesLink.addEventListener("click", function () {
+    window.location.href = "rules.html";});
+    
